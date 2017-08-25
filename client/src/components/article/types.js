64965e62487e8f5b8ed837693b1eb50460a95f24
Alex <toptal.alltime@gmail.com>
2017-08-25 20:09:55 +0800

    Button Type Definition

--- client/src/components/article/types.js
+++ client/src/components/article/types.js
@@ -1,1 +1,3 @@
 export const VOTED_ARR = ['You voted as Sensationalized.', 'You have not voted yet.', 'You voted as Factual.'];
+export const VOTE_NOW = 'Vote Now';
+export const VOTE_AGAIN_BUTTON = 'Vote Again';
